a409615dcc21ef57991f532fcf016559dfdff251
ue-an <fujisakiian@gmail.com>
2023-02-09 22:52:55 +0800

    update app js

--- src/App.js
+++ src/App.js
@@ -24,12 +24,72 @@ function App(props) {
       </form>
       <div className='filters btn-group stack-exception'>
         <button type='button' className='btn toggle-btn' aria-pressed='false'>
-          <span className='visually-hidden'>Show</span>
+          <span className='visually-hidden'>Show </span>
           <span>all</span>
-          <span className='visually-hidden'>tasks</span>
-          <span></span>
+          <span className='visually-hidden'> tasks</span>
+        </button>
+        <button type='button' className='btn toggle-btn' aria-aria-pressed='false'>
+          <span className='visually-hidden'>Show </span>
+          <span>Active</span>
+          <span className='visually-hidden'> tasks</span>
+        </button>
+        <button type='button' className='btn toggle-btn' aria-pressed='false'>
+          <span className='visually-hidden'>Show </span>
+          <span>Completed</span>
+          <span className='visually-hidden'> tasks</span>
         </button>
       </div>
+      <h2 id='list-heading'>3 tasks remaining</h2>
+      <ul role="list" className='todo-list stack-large stack-exception' aria-labelledby='list-heading'>
+      <li className="todo stack-small">
+          <div className="c-cb">
+            <input id="todo-0" type="checkbox" defaultChecked={true} />
+            <label className="todo-label" htmlFor="todo-0">
+              Eat
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn">
+              Edit <span className="visually-hidden">Eat</span>
+            </button>
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden">Eat</span>
+            </button>
+          </div>
+        </li>
+        <li className='todo stack-small'>
+          <div className='c-cb'>
+            <input id='todo-1' type='checkbox'/>
+            <label className='todo-label' htmlFor='todo-1'>
+              Sleep
+            </label>
+          </div>
+          <div className='btn-group'>
+            <button type='button' className='btn'>
+              Edit <span className='visually-hidden'>Sleep</span>
+            </button>
+            <button type='button' className='btn btn__danger'>
+              Delete <span className='visually-hidden'>Sleep</span>
+            </button>
+          </div>
+        </li>
+        <li className='todo stack-small'>
+          <div className='c-cb'>
+            <input id='todo-2' type='checkbox'/>
+            <label className='todo-label' htmlFor='todo-s'>
+              Repeat
+            </label>
+          </div>
+          <div className='btn-group'>
+            <button type='button' className='btn'>
+              Edit <span className='visually-hidden'>Repeat</span>
+            </button>
+            <button type='button' className='btn btn__danger'>
+              Delete <span className='visually-hidden'>Repeat</span>
+            </button>
+          </div>
+        </li>
+      </ul>
     </div>
   );
 }
